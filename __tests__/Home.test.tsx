5c20b7f965222755661737b98bafeb0342027dda
Shivam Sagar <shivamsagar888@gmail.com>
2024-08-03 14:10:12 +0530

    Boilerplate added with tests

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import Home from '@/app/page'
+
+it('should have Docs test', () => {
+    // I have followed AAA approach which is Arrange, Act, Assert
+
+    render(<Home />) // Arrange
+
+    const element = screen.getByRole('heading', {
+        name: "Docs"
+    })
+    
+    expect(element).toBeInTheDocument() // Assert
+})
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h2>
+        Instagram
+      </h2>
+    </main>
+  );
+}
