c4ec65c0405ad159853d395704d745b1005b153f
Shivam Sagar <shivamsagar888@gmail.com>
2024-08-04 23:00:53 +0530

    Minor cleanup

--- app/api/getUsersInstagramStories/route.tsx
+++ app/api/getUsersInstagramStories/route.tsx
@@ -3,7 +3,6 @@ import usersStories from "./db";
 import { UserStories } from "@/types/types";
 
 export async function GET(request: Request) {
-    // Do whatever you want
     const data: UserStories[] = usersStories;
     return NextResponse.json(data, { status: 200 });
 }
